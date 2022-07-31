29cdb8c9fe2d2e9fcd8da6d12a8a4eadbe510350
Thomas Reese <tjrwh2@gmail.com>
2022-07-31 16:19:03 -0700

    Hide items based on min ratings

--- filter.js
+++ filter.js
@@ -8,28 +8,40 @@ const ADS_ITEM_TIP = "tst-ec-white"
 
 const FILTER_SECTION_CLASS = "custom-filter-section"
 
+const DEFAULT_MIN_RATING = 1
+
 const maybeInsertAfterPageHeader = (element) => {
 	const header = document.getElementsByClassName(INGRESS_HEADER_CLASS)[0]
-	console.log('header', header)
 	header?.after(element)
 }
 
 const createFilters = () => {
 	const onChangeFilters = () => {
-		adjustCardsBasedOnFilters(paidCheck.checked, freeCheck.checked, adsCheck.checked)
+		adjustCardsBasedOnFilters(paidCheck.checked, freeCheck.checked, adsCheck.checked, parseInt(minRating.value))
 	}
-	const container = html.div(FILTER_SECTION_CLASS)
+
 	const paidCheck = html.check(true, onChangeFilters)
 	const freeCheck = html.check(true, onChangeFilters)
 	const adsCheck = html.check(true, onChangeFilters)
-	container.append(
+	const minRatingSet = html.title(`(${DEFAULT_MIN_RATING})`)
+	const minRating = html.range(DEFAULT_MIN_RATING, 1, 100, (a) => {
+		console.log(minRating, a)
+		onChangeFilters()
+	})
+	minRating.addEventListener(C.Action.Input, ({currentTarget}) => {
+		minRatingSet.innerText = `(${currentTarget.value})`
+	})
+	const filterRow = html.flexRow(
 		html.title("Paid: "), paidCheck,
 		html.title("Free: "), freeCheck,
-		html.title("Ads: "), adsCheck
+		html.title("Ads: "), adsCheck,
+		html.title("Min Rating: "), minRating, minRatingSet
 	);
+
+	const titleRow = html.flexRow(html.h2('Filters'), html.img(C.Icon.LOGO, 20))
+	const container = html.flexColumn(titleRow, filterRow)
+	container.className = FILTER_SECTION_CLASS
 	html.styles(container, {
-		"display": "flex",
-		"gap": "20px",
 		"align-items": "baseline",
 		"margin-left": "50px",
 		"padding-top": "24px",
@@ -54,28 +66,69 @@ const setDisplayedIB = (item, visible) => {
 	html.style(item, 'display', visible ? 'inline-block' : 'none')
 }
 
+const ratingsMeetBar = (minRating, ratings) => {
+	if (!ratings) return
+	const { metacritic, imdb, rottenTomatoes } = ratings
+	if (metacritic === '-' && imdb === '-' && rottenTomatoes === '-') {
+		return true
+	}
+
+	if (metacritic !== '-' && parseFloat(metacritic) >= minRating) {
+		return true
+	}
+	if (imdb !== '-' && parseFloat(imdb) * 10 >= minRating) {
+		return true
+	}
+	if (rottenTomatoes !== '-' && parseFloat(rottenTomatoes) >= minRating) {
+		return true
+	}
+
+	return false
+}
+
 // TODO: Run this on any newly added cards
 // TODO: Make these properties global
 // TODO: Add these to settings
-const adjustCardsBasedOnFilters = (paid, free, ads) => {
+const adjustCardsBasedOnFilters = (paid, free, ads, minRating) => {
 	const innerCards = document.getElementsByClassName(TOP_IMAGE_WRAPPER_CLASS)
-	console.log(innerCards)
+	const hashKeys = Object.keys(settings.get(Setting.OmdbResultsHash))
 	for (const card of innerCards) {
 		const priceTip = card.getElementsByClassName(ITEM_PRICE_TIP)[0]
 		const list = priceTip?.classList
 		if (!list) continue
+
 		const wrapper = findParentByTagName(card, 'li')
-		if (list.contains(PAID_ITEM_TIP)) {
-			setDisplayedIB(wrapper, paid)
+		if (!paid && list.contains(PAID_ITEM_TIP)) {
+			setDisplayedIB(wrapper, false)
 			continue
 		}
-		else if (list.contains(FREE_ITEM_TIP)) {
-			setDisplayedIB(wrapper, free)
+		else if (!free && list.contains(FREE_ITEM_TIP)) {
+			setDisplayedIB(wrapper, false)
 			continue
 		}
-		else if (list.contains(ADS_ITEM_TIP)) {
-			setDisplayedIB(wrapper, ads)
+		else if (!ads && list.contains(ADS_ITEM_TIP)) {
+			setDisplayedIB(wrapper, false)
 			continue
 		}
+
+		if (minRating > 1) {
+			const ariaTitle = card?.firstElementChild?.ariaLabel
+			if (ariaTitle) {
+				const title = cleanTitle(ariaTitle)
+				if (title) {
+					const matchingKey = hashKeys.find(key => key.startsWith(`${title}|`))
+					if (matchingKey) {
+						const ratings = getRatings(matchingKey)
+						if (ratings) {
+							const meets = ratingsMeetBar(minRating, ratings)
+							setDisplayedIB(wrapper, meets)
+							continue
+						}
+					}
+				}
+			}
+		}
+
+		setDisplayedIB(wrapper, true)
 	}
 }
--- htmlElements.js
+++ htmlElements.js
@@ -57,14 +57,30 @@ const html = (() => {
 		return check
 	}
 
-	const imgButton = (src = "", fn = C.Noop, size) => {
+	const range = (initialValue, min, max, fn = C.Noop) => {
+		const range = document.createElement("input")
+		range.type = "range"
+		range.min = min
+		range.max = max
+		range.value = initialValue
+		// html.styles(range, { height: "20px", width: "20px", outline: "3px solid var(--nc-lk-1)", "border-radius": "5px", "outline-style": "auto" })
+		if (typeof fn === "function") range.addEventListener(C.Action.Change, ({currentTarget}) => fn(currentTarget.value), false)
+		return range
+	}
+
+	const img = (src = "", size) => {
 		const img = document.createElement("img")
 		img.src = src
-		if (typeof fn === "function") img.addEventListener(C.Action.Click, fn, false)
 		if (size) {
 			img.height = size
 			img.width = size
 		}
+		return img
+	}
+
+	const imgButton = (src = "", fn = C.Noop, size) => {
+		const img = html.img(src, size)
+		if (typeof fn === "function") img.addEventListener(C.Action.Click, fn, false)
 		return img
 	}
 
@@ -79,12 +95,38 @@ const html = (() => {
 		if (className) span.className = className
 		return span
 	}
+
+	const flexDiv = (...children) => {
+		const flex = html.div()
+		html.styles(flex, {
+			"display": "flex",
+			"gap": "10px",
+		})
+		flex.append(...children)
+		return flex
+	}
+	const flexRow = (...children) => {
+		const flex = flexDiv(...children)
+		html.styles(flex, { "flex-direction": "row" })
+		return flex
+	}
+	const flexColumn = (...children) => {
+		const flex = flexDiv(...children)
+		html.styles(flex, { "flex-direction": "column" })
+		return flex
+	}
+
 	const kbd = (text) => {
 		const kbd = document.createElement("kbd")
 		kbd.innerText = text
 		return kbd
 	}
-
+	const h2 = (text) => {
+		const h2 = document.createElement("h2")
+		h2.innerText = text
+		html.styles(h2, {"font-size": "19px", "font-weight": "700", "line-height": "24px"})
+		return h2
+	}
 	const title = (text) => {
 		const title = document.createElement("p")
 		title.innerText = text
@@ -103,10 +145,15 @@ const html = (() => {
 		br,
 		div,
 		span,
+		flexRow,
+		flexColumn,
 		check,
+		range,
+		img,
 		imgButton,
-		kbd,
 
+		kbd,
+		h2,
 		title,
 	}
 	return publicApi
